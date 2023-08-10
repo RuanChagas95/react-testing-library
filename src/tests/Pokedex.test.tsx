57a0ae64ee9a0bdfce4696ff1370716da19b9667
Ruan Chagas <ruanchagas95@gmail.com>
2023-08-10 19:14:32 -0300

    5 - complete

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+it('A página contém um h2 com o \'texto Encoutered Pokémon\'', () => {
+  renderWithRouter(<App />);
+  screen.getByRole('heading', { level: 2, name: /Encountered Pokémon/i });
+});
+
+it('Ao clickar no botão o próximo pokémon da lista deve ser mostrado', async () => {
+  renderWithRouter(<App />);
+  const button = screen.getByRole('button', { name: /Próximo pokémon/i });
+  screen.getByText(/pikachu/i);
+  await userEvent.click(button);
+  screen.getByText(/charmander/i);
+});
