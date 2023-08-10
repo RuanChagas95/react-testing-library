fb8ec6a844a042a2c4b77bde3c578577555fd69d
Ruan Chagas <ruanchagas95@gmail.com>
2023-08-10 18:57:52 -0300

    4 - complete

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,15 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('NotFound tests', () => {
+  it('Existe um h2 com o texto \'Page requested not found\'', () => {
+    render(<NotFound />);
+    screen.getByRole('heading', { level: 2, name: /Page requested not found/i });
+  });
+
+  it('Existe uma imagem com o atributo src contendo o valor \'/404.gif\'', () => {
+    render(<NotFound />);
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', '/404.gif');
+  });
+});
